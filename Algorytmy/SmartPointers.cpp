97f138b64f5d864ab377c44af3636a9384a53395
Szymon Bortel <szymonbortel8@gmail.com>
2018-03-22 20:20:53 +0100

    wyrazenia lambda,lista, task z codility

---
+++ Algorytmy/SmartPointers.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "SmartPointers.h"
+
+
+SmartPointers::SmartPointers()
+{
+}
+
+
+SmartPointers::~SmartPointers()
+{
+}
---
+++ Algorytmy/SmartPointers.h
@@ -0,0 +1,8 @@
+#pragma once
+class SmartPointers
+{
+public:
+	SmartPointers();
+	~SmartPointers();
+};
+
